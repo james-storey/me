03ee952817755812a8eeef0f65034d641007b496
James Storey <james@sysreturn.net>
2018-05-27 17:54:28 -0500

    set secondary color, adjust footer colors

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,4 +1,4 @@
-<template lang='pug'>
+<template lang="pug">
 footer.footer
   .container
     .content.has-text-centered
@@ -8,3 +8,10 @@ footer.footer
         | is licensed <a href="https://opensource.org/licenses/mit-license.php">MIT</a>.
         | The website content is licensed <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/"> CC BY NC SA 4.0</a>.
 </template>
+
+<style lang="scss">
+  @import "../_variables.scss";
+  .footer {
+    color: $grey-light
+  }
+</style>
